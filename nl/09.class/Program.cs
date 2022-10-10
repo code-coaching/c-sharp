807311fb28fd5ada846815d395ea8cfa3cf6666d
Bart Duisters <bartduisters@bartduisters.com>
2022-10-10 14:57:09 +0200

    feat: method overloading

--- nl/09.class/Program.cs
+++ nl/09.class/Program.cs
@@ -2,8 +2,10 @@
 
 Rekenmachine rekenmachine = new Rekenmachine(); // Instantie van de class Rekenmachine
 // Methodes aanroepen op de instantie
-int som = rekenmachine.Som(6, 9);
 int verschil = rekenmachine.Verschil(6, 9);
+int som = rekenmachine.Som(6, 9); // Method Som met signature Som(int, int)
+int som2 = rekenmachine.Som("3", "6"); // Method Som met signature Som(string, string)
+int som3 = rekenmachine.Som(3); // Method Som met signature Som(int)
 
 // Static methodes aanroepen direct op de class (geen instantie nodig)
 int optellen = Rekenmachine.Optellen(6, 9);
@@ -15,6 +17,18 @@ class Rekenmachine
     public int Som(int a, int b)
     {
         return a + b;
+    }
+
+    // Method overloading: dezelfde functienaam, maar andere type parameters 
+    public int Som(string a, string b)
+    {
+        return int.Parse(a) + int.Parse(b);
+    }
+
+    // Method overloading: dezelfde functienaam, maar andere hoeveelheid parameters
+    public int Som(int a)
+    {
+        return a + a;
     }
 
     public int Verschil(int a, int b)
